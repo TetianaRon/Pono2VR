25f61ae7417003e86514c242dad88bd57f9acc05
Yuriy Paramonov <yuriy.paramonov@outlook.com>
2021-11-16 11:27:43 +0200

    Add WebGL responsive UI

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjectSpawner.cs" company="Supyrb">
+//   Copyright (c) 2019 Supyrb. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject prefab = null;
+
+	[SerializeField]
+	private float spawnCooldownSeconds = 0.5f;
+
+	[SerializeField]
+	private int maxInstances = 200;
+	
+	private int instances = 0;
+	private Queue<GameObject> spawnedObjects = null;
+	private float lastSpawnTime;
+
+	void Awake()
+	{
+		spawnedObjects = new Queue<GameObject>(maxInstances + 5);
+		lastSpawnTime = Time.time;
+	}
+
+	void Update()
+	{
+		if (lastSpawnTime + spawnCooldownSeconds <= Time.time)
+		{
+			SpawnObject();
+			lastSpawnTime = Time.time;
+		}
+	}
+
+	private void SpawnObject()
+	{
+		if (instances >= maxInstances)
+		{
+			var recycleGo = spawnedObjects.Dequeue();
+			recycleGo.transform.localPosition = transform.position;
+			recycleGo.transform.localRotation = transform.localRotation;
+			spawnedObjects.Enqueue(recycleGo);
+			return;
+		}
+
+		var newGo = Instantiate(prefab, transform.position, transform.rotation);
+		spawnedObjects.Enqueue(newGo);
+		instances++;
+	}
+
+	#if UNITY_EDITOR
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawWireSphere(transform.position, 0.5f);
+	}
+	#endif
+}
