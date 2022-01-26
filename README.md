ee8962085dc354a5f42f41c20a549936bb6e61ec
Yuriy Paramonov <yuriy.paramonov@outlook.com>
2022-01-26 07:38:54 +0200

    Add notes on auto upload to Readme

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 ## Feture request
 
-
  - 1. Language switch ~ 15 minutes
  - 2. City switch ~ 15 minutes
  - 3. Virtual Walk ~ 30 minutes
+
+
+## Upload to cloud
+
+To upload run command:
+
+`.\UploadScript.ps1 "{VERSION}"`
+
+> `{VERSION}` - current name of apk for example {1.14}.apk
+
+
 
--- UploadScript.ps1
+++ UploadScript.ps1
@@ -1,4 +1,8 @@
 	$build=$args[0]
+	echo $("Sart upload for  build:"+$build)
+
 	$file = "B:\Git\Pono2VR\Builds\Oculus\"+$build;
-	B:\Res\AppData\Roaming\odh\ovr-platform-util.exe upload-quest-build --app-id 4778895315525656 --app-secret 4c97715bde301d62ad12fb61b7b90c2c --apk $($file+".apk")  --channel LIVE --obb $($file+".main.obb")
-	echo $file
+	$apk = $($file+".apk") 
+	B:\Res\AppData\Roaming\odh\ovr-platform-util.exe upload-quest-build --app-id 4778895315525656 --app-secret 4c97715bde301d62ad12fb61b7b90c2c --apk $apk --channel LIVE --obb $($file+".main.obb")
+	echo $("Uploaded file: "+$file)
+
